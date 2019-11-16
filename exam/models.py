2940827cabf0a401a839fb0a65f872e2eb72d244
hyerimsn <etcheef@likelion.org>
2019-11-16 16:15:03 +0900

    hyerimsn3

--- exam/models.py
+++ exam/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Seed(models.Model):
     name = models.TextField(max_length=20)
-    age = models.TextField(max_length=20)
+    age = models.TextField(max_length=20) #여기서 age가 non-nullable 하다는데 잘 모르겠어유,,,,ㅎㅎ,,,
     major = models.TextField(max_length=20)
 
 class Answer(models.Model):
